7ac5366c61d3e4c3d8d06dfa42bccfc21b0605e5
mokeshtechie <mmokesh0@gmail.com>
2024-03-29 05:17:46 -0700

    all done

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,9 +1,14 @@
-import React,{useRef,useEffect} from 'react'
+import React,{useRef,useEffect,useState} from 'react'
 
 function Header() {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
-  const stickyHeaderFunc = ()=>{
+  const [nav,setNav] = useState(false)
+  const Handle = ()=>{
+    setNav(!nav)
+  }
+ 
+ const stickyHeaderFunc = ()=>{
     window.addEventListener('scroll',()=>{
       if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
         headerRef.current.classList.add('sticky_header')
@@ -63,28 +68,43 @@ const toggleMenu = () => {
           </div>
        
 <div>
-<div className='menu' onClick={toggleMenu} Ref={menuRef}>
-  <ul className='flex gap-10 item-center'>
+<div className='' onClick={toggleMenu} Ref={menuRef}>
+  <ul className='md:flex gap-10 item-center hidden '>
     <li>    <a onClick={handleClick} className='font-[500] text-purple-400' href='#about'>about </a></ li>
     <li>  <a  onClick={handleClick} className='font-[500] text-purple-400' href='#services'>services</a></li>
     <li> <a onClick={handleClick}  className='font-[500] text-purple-400'href='#portfolio'>portfolio </a></li>
     <li>  <a  onClick={handleClick} className='font-[500] text-purple-400' href='#contact'>contact</a></li>
   </ul> 
+ 
 </div>
 </div>
 <div className='p-5 flex gap-7'>
 <button className=' flex items-center gap-4 text-black  border border-solid border-black px-2 h-[50px]  hover:text-white  hover:font-[500] hover:bg-slate-900 rounded-xl ease-in duration-300'>
   <i ><svg className='h-[25px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z"></path></svg></i>  <p classname="text-xl">lets talk </p>
 </button>
-<span  className='text-2xl text-slate-950 cursor-pointer'>
-
-<svg  onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='w-[30px] h-[30px] mt-3'><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path></svg>
-</span>
 </div>
+<div className='md:hidden'  onClick={Handle}>
+  {!nav ? <i className='fas fa-times'></i> :<i className='fas fa-bars'></i> }
 
 </div>
+
+  </div>
+  <div className='md:hidden'>
+  <div className={!nav?'fixed w-[60%] h-full top-0 left-0 bg-white text-voilet-600 text-[30px] font-[500]':'fixed left-[-100%]'}>
+<ul className='flex gap-8 item-center flex-col  p-14'>
+    <li>    <a onClick={handleClick} className='font-[500] text-purple-400' href='#about'>about </a></ li>
+    <li>  <a  onClick={handleClick} className='font-[500] text-purple-400' href='#services'>services</a></li>
+    <li> <a onClick={handleClick}  className='font-[500] text-purple-400'href='#portfolio'>portfolio </a></li>
+    <li>  <a  onClick={handleClick} className='font-[500] text-purple-400' href='#contact'>contact</a></li>
+  </ul> 
+</div>
+  </div>
+
 </div>
 </div>
+
+
+
   )
 }
 
